87d3b1d30b4ce3349c098ca856ba55c0f276f1eb
fred <frednobre18@gmail.com>
2025-06-16 21:34:31 +0200

    update checkNumber method and have method check firstName

--- src/test/java/org/example/service/ContactManagerTest.java
+++ src/test/java/org/example/service/ContactManagerTest.java
@@ -5,19 +5,33 @@ import org.junit.Test;
 
 import java.nio.file.Path;
 
-public class ContactManagerTest {
+import static org.junit.Assert.assertEquals;
+
+public class ContactManagerTest { // todo Renomer en contact test car je ne test pas les methode de contactManager
     @Test
     public void thePhoneNumberMustHave10Numbers() throws Exception {
         //GIVEN
+        String resutl;
         Path filePath = Path.of("contact.txt");
-        String wrongPhoneNumber = "1212121212";
+        String wrongPhoneNumber = "1010101010000";
         Contact testContact = new Contact("Robert", "Boulard", "rob@gmail.com", wrongPhoneNumber, filePath);
         //WHEN
-        String callTheMethod = testContact.getPhone();
-        if (callTheMethod.length() != 10) {
-            throw new Exception("A phone number must have 10 numbers");
-        }
+        resutl = testContact.checkPhoneNumber();
         //THEN
+        assertEquals("A phone number must have 10 numbers", resutl);
+    }
+
+    @Test
+    public void aUserMustHaveAFirstName() throws Exception {
+        //GIVEN
+        Path filePath = Path.of("contact.txt");
+        String userWithNoFirstName = null;
+        String userWithLastName = null;
+        Contact testContact = new Contact(userWithNoFirstName, userWithLastName, "empty@Gmail.com", "0192837465", filePath);
+        //WHEN
+        String resutl = testContact.checkFirstName();
+        //THEN
+        assertEquals("First name missing !!!", resutl);
     }
 
 }
