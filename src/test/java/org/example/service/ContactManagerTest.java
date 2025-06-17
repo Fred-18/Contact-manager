b7b686d10a52fae9815c08604758998926b9f0f9
fred <frednobre18@gmail.com>
2025-06-17 20:04:39 +0200

    add new test🧪

--- src/test/java/org/example/service/ContactManagerTest.java
+++ src/test/java/org/example/service/ContactManagerTest.java
@@ -2,38 +2,40 @@ package org.example.service;
 
 import org.example.model.Contact;
 import org.junit.Test;
-
 import java.nio.file.Path;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static org.junit.Assert.assertEquals;
+public class ContactManagerTest { // todo rename en contact test car je ne test pas les methode de contactManage
 
-public class ContactManagerTest { // todo Renomer en contact test car je ne test pas les methode de contactManager
     @Test
-    public void thePhoneNumberMustHave10Numbers() throws Exception {
+    public void thePhoneNumberMustHave10Numbers() {
         //GIVEN
-        String resutl;
+        String result;
         Path filePath = Path.of("contact.txt");
         String wrongPhoneNumber = "1010101010000";
-        Contact testContact = new Contact("Robert", "Boulard", "rob@gmail.com", wrongPhoneNumber, filePath);
-        //WHEN
-        resutl = testContact.checkPhoneNumber();
-        //THEN
-        assertEquals("A phone number must have 10 numbers", resutl);
+        Contact testContact = new Contact("Robert", "Boulard", "rob@gmail.com", wrongPhoneNumber, filePath);//WHEN
+        //WHEN+THEN
+        assertThrows(IllegalArgumentException.class, testContact::checkPhoneNumber);
     }
 
     @Test
-    public void aUserMustHaveAFirstName() throws Exception {
+    public void aUserMustHaveAFirstName() {
         //GIVEN
         Path filePath = Path.of("contact.txt");
-        String userWithNoFirstName = null;
-        String userWithLastName = null;
-        Contact testContact = new Contact(userWithNoFirstName, userWithLastName, "empty@Gmail.com", "0192837465", filePath);
-        //WHEN
-        String resutl = testContact.checkFirstName();
-        //THEN
-        assertEquals("First name missing !!!", resutl);
+        Contact testContact = new Contact(null, null, "empty@Gmail.com", "0192837465", filePath);
+        //WHEN+THEN
+        assertThrows(IllegalArgumentException.class, testContact::checkFirstName);
     }
 
+
+    @Test
+    public void aUserShouldHaveALastName() {
+        //GIVEN
+        Path filePath = Path.of("contact.txt");
+        Contact testContact = new Contact(null, null, "empty@Gmail.com", "0192837465", filePath);
+        //WHEN+THEN
+        assertThrows(IllegalArgumentException.class, testContact::checkLastName);
+    }
 }
 
 
